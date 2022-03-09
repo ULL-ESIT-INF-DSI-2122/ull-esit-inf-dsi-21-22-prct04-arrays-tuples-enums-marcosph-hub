ff2ba6c5955e47df7cf71eeba330b708c49f59c2
Marcos Padilla <alu0101045177@ull.edu.es>
2022-03-09 19:42:44 +0000

    tests files

--- test/multiply.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import { expect } from 'chai';
-import { productTable } from '../src/multiply';
-
-describe('Test', () => {
-  it ('Test2', () =>{
-    expect(productTable(2)).to.be.eq(4);
-  });
-});
